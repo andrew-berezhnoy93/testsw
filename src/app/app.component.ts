3e8a18466ae566b798e060b1d650bd2c5b9b04ff
Passwordis1 <passwordis1@Passwordis1s-MacBook-Pro.local>
2018-02-16 03:35:50 +0200

    sw/chat/manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  AngularFireDatabase,
+  FirebaseObjectObservable,
+  FirebaseListObservable
+} from 'angularfire2/database-deprecated';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  messages: FirebaseListObservable<any[]>;
+
+  message: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    text: new FormControl(null, Validators.required)
+  });
+  constructor(db: AngularFireDatabase) {
+    this.messages = db.list('messages');
+  }
+
+  send(event) {
+    this.messages.push({
+      ...event,
+      date: new Date().toLocaleTimeString()
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDmlg--76FbxpO5CS-NaMgV9XyT7OfCfGA',
+      authDomain: 'ngsw-firechat.firebaseapp.com',
+      databaseURL: 'https://ngsw-firechat.firebaseio.com',
+      projectId: 'ngsw-firechat',
+      storageBucket: 'ngsw-firechat.appspot.com',
+      messagingSenderId: '602956867256'
+    }),
+    AngularFireDatabaseModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
